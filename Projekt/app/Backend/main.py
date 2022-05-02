223964b5a8d12fb983905b739f977cc8bae73e0a
yauza <jan.stobnicki@risco.net.pl>
2022-05-02 14:10:58 +0200

    added init.sh script for installing python and required packages (if needed) + added api for data requests
    todo: check for python version upgrade (ex. 3.5 -> 3.9), angular dependencies

---
+++ Projekt/app/Backend/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse, JSONResponse
+from utils import get_json_object_from_file
+
+RESOURCES_PATH = "../resources/"
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def get_main_page():
+    page_content = """
+        <h1>IO DataHub</h1>
+    """
+    return HTMLResponse(content=page_content, status_code=200)
+
+@app.get("/api/general_info", response_class=JSONResponse)
+def get_general_info():
+    try:
+        general_info = get_json_object_from_file(RESOURCES_PATH + "general_info.json")
+    except FileNotFoundError as err:
+        raise HTTPException(status_code=404, detail=err.strerror)
+    return JSONResponse(content=general_info, status_code=200)
+
+@app.get("/api/screen/{screen_id}", response_class=JSONResponse)
+def get_screen_info(screen_id: int):
+    file_name = "screen" + str(screen_id) + ".json"
+    try:
+        screen_info = get_json_object_from_file(RESOURCES_PATH + file_name)
+    except FileNotFoundError as err:
+        raise HTTPException(status_code=404, detail=err.strerror)
+    return JSONResponse(content=screen_info, status_code=200)
+
+@app.get("/api/chart/{chart_id}/data/{data_id}", response_class=JSONResponse)
+def get_data(chart_id: int, data_id: int):
+    file_name = "chart_" + str(chart_id) + "_data_" + str(data_id) + ".json"
+    try:
+        js = get_json_object_from_file(RESOURCES_PATH + file_name)
+    except FileNotFoundError as err:
+        raise HTTPException(status_code=404, detail=err.strerror)
+    return JSONResponse(content=js, status_code=200)
